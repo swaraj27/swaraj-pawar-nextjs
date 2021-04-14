8e10e9f4cc49ae0a0511c3479bb714f488b360ec
Swaraj Rajendra Pawar <swarajpawar27@gmail.com>
2021-04-14 15:24:58 +0530

    about us

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export const about = () => {
             <p className="text-darkGray   items-center justify-center md:text-xl text-sm    py-2">Hi, I'm Swaraj Pawar, 25 years old. living in Mumbai & working as Software Developer.</p>
             <p className="text-darkGray   items-center justify-center md:text-xl text-sm    py-2">After getting my Bachelors degree in Computer Engineering I decided to become a full-time full stack developer. Late 2019 , I began working as software developer at MTCLLP, a medical domain advertising agency.  </p>
             
-            <Image className="items-center justify-center rounded shadow " src="/swa-raj.jpg" alt="picture of author" width="full" height={400}/>
+            {/* <Image className="items-center justify-center rounded shadow " src="/swa-raj.jpg" alt="picture of author" width="full" height={400}/> */}
             <p className="text-darkGray   items-center justify-center md:text-xl text-sm    py-2">Apart from desing and code , I love playing cricket, online games for fun or watching movies and web series. I also love Cofee</p>
             </div>
       </div>
