c0390cb8adc334e3186b1a752b959a53066fb722
Swaraj Rajendra Pawar <swarajpawar27@gmail.com>
2021-04-14 16:01:04 +0530

    Add image

--- pages/about.js
+++ pages/about.js
@@ -13,6 +13,7 @@ export const about = () => {
             <p className="text-darkGray   items-center justify-center md:text-xl text-sm    py-2">After getting my Bachelors degree in Computer Engineering I decided to become a full-time full stack developer. Late 2019 , I began working as software developer at MTCLLP, a medical domain advertising agency.  </p>
             
             {/* <Image className="items-center justify-center rounded shadow " src="/swa-raj.jpg" alt="picture of author" width="full" height={400}/> */}
+            <img src="/swaraj.jpg" alt="picture of author " className="w-full border border-black rounded "/>
             <p className="text-darkGray   items-center justify-center md:text-xl text-sm    py-2">Apart from desing and code , I love playing cricket, online games for fun or watching movies and web series. I also love Cofee</p>
             </div>
       </div>
