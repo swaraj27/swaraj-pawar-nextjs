5a1cc17b69ec0afac028cc673a880d585cc58c4a
Swaraj Rajendra Pawar <swarajpawar27@gmail.com>
2021-06-11 23:36:56 +0530

    Added Hover effct

--- pages/index.js
+++ pages/index.js
@@ -12,28 +12,28 @@ export default function Home() {
    
     <div className="flex-none md:flex md:my-4 py-4 text-primaryGray px-4 md:px-12">
 
-      <div className="md:flex-1 rounded  border border-primaryGray my-4 md:my-0  md:px-10  md:py-4 md:mx-10 "> 
-        <p className="text-lg text-primaryGreen font-semibold py-2 px-2">Web App Design</p>
-        <p className="text-darkGray text-sm md:text-base py-2 px-2"> Whether it's designing a new product or refreshing the design of an existing application, I'm happy to help making project beautiful and easy to use. </p>
+      <div className="group md:flex-1 rounded  border border-primaryGray my-4 md:my-0  md:px-10  md:py-4 md:mx-10 hover:bg-primaryGray"> 
+        <p className="text-lg text-primaryGreen font-semibold py-2 px-2 group-hover:text-primaryBackround">Web App Design</p>
+        <p className="text-darkGray text-sm md:text-base py-2 px-2 group-hover:text-primaryBackround"> Whether it's designing a new product or refreshing the design of an existing application, I'm happy to help making project beautiful and easy to use. </p>
       </div>
       
-      <div className="md:flex-1 rounded  border border-primaryGray md:px-10 md:py-4 md:mx-10 ">
-        <p className="text-lg text-primaryGreen font-semibold py-2 px-2">Front-end Development</p>
-        <p className="text-darkGray text-sm  md:text-base py-2 px-2">Modern browsers offer a lot of possibilities but also demand for specialized approach. I translate designs to production ready HTML, CSS and Javascript and <span className="text-primaryGreen">React</span> complete my frontend stack.</p>
+      <div className="group md:flex-1 rounded  border border-primaryGray md:px-10 md:py-4 md:mx-10 hover:bg-primaryGray ">
+        <p className="text-lg text-primaryGreen font-semibold py-2 px-2 group-hover:text-primaryBackround">Front-end Development</p>
+        <p className="text-darkGray text-sm  md:text-base py-2 px-2 group-hover:text-primaryBackround">Modern browsers offer a lot of possibilities but also demand for specialized approach. I translate designs to production ready HTML, CSS and Javascript and React complete my frontend stack.</p>
       </div>
     
     </div>
     
     <div className="flex-none  md:flex md:my-4  text-primaryGray px-4 md:px-12">
-      <div className=" md:flex-1 rounded border border-primaryGray   md:px-10  md:py-4 md:mx-10"> 
-        <p className="text-lg text-primaryGreen font-semibold py-2 px-2">Mobile Design</p>
-        <p className="text-darkGray text-sm  md:text-base py-2 px-2"> I can developed and design mobile apps for android. I can use microservices for app devlopment. I used <span className="text-primaryGreen">Java</span>  for app development and design.
+      <div className="group md:flex-1 rounded border border-primaryGray   md:px-10  md:py-4 md:mx-10 hover:bg-primaryGray"> 
+        <p className="text-lg text-primaryGreen font-semibold py-2 px-2 group-hover:text-primaryBackround">Mobile Design</p>
+        <p className="text-darkGray text-sm  md:text-base py-2 px-2 group-hover:text-primaryBackround"> I can developed and design mobile apps for android. I can use microservices for app devlopment. I used Java  for app development and design.
         </p>
       </div>
 
-      <div className=" md:flex-1 rounded  border border-primaryGray my-4 md:my-0 md:px-10  md:py-4 md:mx-10">
-      <p className="text-lg text-primaryGreen font-semibold py-2 px-2">Rest API's</p>
-      <p className="text-darkGray text-sm md:text-base py-2 px-2">I'm  using Rest Api's for backend development. I Prefered Java for backend. I used <span className="text-primaryGreen"> Spring Boot</span> framework  for developing microservices.</p>
+      <div className="group md:flex-1 rounded  border border-primaryGray my-4 md:my-0 md:px-10  md:py-4 md:mx-10 hover:bg-primaryGray">
+      <p className="text-lg text-primaryGreen font-semibold py-2 px-2 group-hover:text-primaryBackround">Rest API's</p>
+      <p className="text-darkGray text-sm md:text-base py-2 px-2 group-hover:text-primaryBackround">I'm  using Rest Api's for backend development. I Prefered Java for backend. I used  Spring Boot framework  for developing microservices.</p>
       </div>
     </div>
     </div>
